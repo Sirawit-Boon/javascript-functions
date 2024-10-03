37092abc0592df0076d44c35a087515de0133536
TheYami <luffy90san@gmail.com>
2024-10-03 13:21:15 +0700

    fear/ex2

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,21 @@
 // Start coding here
+const add = (a,b) => {
+    return a+b;
+}
+
+const subtract = (a,b) => {
+    return a-b;
+}
+
+const multiply = (a,b) => {
+    return a*b;
+}
+
+const devide = (a,b) => {
+    return a/b;
+}
+
+console.log(add(10,5));
+console.log(subtract(10,5));
+console.log(multiply(10,5));
+console.log(devide(10,5));
