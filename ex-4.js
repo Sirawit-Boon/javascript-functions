e7c02f4971b2109efe64b264916bb8137c5cda62
TheYami <luffy90san@gmail.com>
2024-10-03 13:30:43 +0700

    feat/ex4

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,13 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+    add : (a,b) => a+b,
+    subtract : (a,b) => a-b,
+    multiply : (a,b) => a*b,
+    devide : (a,b) => a/b
+};
+
+const addResult = calculator.add(10,20);
+console.log(addResult);
+
+const devideResult = calculator.devide(3000,10);
+console.log(devideResult);
