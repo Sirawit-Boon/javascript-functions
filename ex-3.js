bdf22aa96ef41a02585dfe8f35027c9e9f2b18e6
Sirawit-Boon <ter.sirawit1997@gmail.com>
2024-10-03 12:12:23 +0700

    assignment function ex1-6

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,16 @@
 // Start coding here
+const add = (a, b) => a + b;
+
+console.log(add(10, 5));
+
+const substract = (a, b) => a - b;
+
+console.log(substract(10, 5));
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(10, 5));
+
+const divide = (a, b) => a / b;
+
+console.log(divide(10, 5));
--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,24 @@
 // Start coding here
-let calculator = {};
+const add = (a, b) => a + b;
+
+const substract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+
+let calculator = {
+    add: add(10, 20),
+    substract: substract(),
+    multiply: multiply(),
+    divide: divide(3000, 10),
+};
+
+
+
+const addResult = calculator.add;
+console.log(addResult);
+
+const divideResult = calculator.divide;
+console.log(divideResult);
