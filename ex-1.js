5a381a7ce6b6a862c97ccabcc8a9d7289c3e7d2d
TheYami <luffy90san@gmail.com>
2024-10-03 13:17:32 +0700

    feat/ex1

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,21 @@
 // Start coding here
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function devide(a,b){
+    return a/b;
+}
+
+console.log(add(10,5));
+console.log(subtract(10,5));
+console.log(multiply(10,5));
+console.log(devide(10,5));
